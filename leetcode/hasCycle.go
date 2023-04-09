77d91f27a872ae4c92e6c8bf27661486916893bf
andong <948031228@qq.com>
2023-04-09 23:00:09 +0800

    环形链表

---
+++ leetcode/hasCycle.go
@@ -0,0 +1,17 @@
+package leetcode
+
+// 环形链表
+// https://leetcode.cn/problems/linked-list-cycle/
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	for slow, quick := head, head.Next; quick != nil && quick.Next != nil; {
+		if slow == quick {
+			return true
+		}
+		slow = slow.Next
+		quick = quick.Next.Next
+	}
+	return false
+}
---
+++ leetcode/hasCycle_test.go
@@ -0,0 +1,20 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHasCycle(t *testing.T) {
+	cycleIn := &ListNode{Val: 2}
+	head := &ListNode{Val: 3, Next: cycleIn}
+	cycleIn.Next = &ListNode{
+		Val: 0,
+		Next: &ListNode{
+			Val:  -4,
+			Next: cycleIn,
+		},
+	}
+	assert.True(t, hasCycle(head))
+}
