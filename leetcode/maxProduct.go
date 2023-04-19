efc446cac1a0bd8b6681c7f5ca41ca62cf9f193d
andong <948031228@qq.com>
2023-04-19 09:17:52 +0800

    不成功第二次提交

--- leetcode/maxProduct.go
+++ leetcode/maxProduct.go
@@ -3,27 +3,35 @@ package leetcode
 // 乘积最大子数组
 // https://leetcode.cn/problems/maximum-product-subarray/
 func maxProduct(nums []int) int {
-	maxNum, minNegative, indexNegative := 0, 0, -1
-	temp := 1
+	maxNum, temp := 0, 1
+	firstNegativeNum, endNegativeNum := 0, 0
 	for i := 0; i < len(nums); i++ {
-		if temp *= nums[i]; temp > maxNum {
-			maxNum = temp
-		}
-		if nums[i] < 0 && indexNegative = -1 {
-
-		}
-		if nums[i] == 0 {
-			if maxNum > 0 {
-				// 啥也不用做哦
+		if nums[i] != 0 {
+			if temp *= nums[i]; temp > maxNum {
+				endNegativeNum = 0
+				maxNum = temp
 			}
-			// 寻找左数组乘积最大数组
-			if temp < 0 {
-				// 要看负数数量是否 > 1 呢
-				// 是：   比较 [0:firt] 的乘积和[end:0]的乘积谁更小
-				// 否：   比较 [0:first] 的乘积 [first + 1:0]的乘积谁更大
+		}
+		if temp < 0 {
+			endNegativeNum *= nums[i]
+			if nums[i] < 0 {
+				if firstNegativeNum == 0 {
+					firstNegativeNum = temp
+				} else {
+					endNegativeNum = nums[i]
+				}
 			}
+		}
 
-			// 将前后 index 为 -1
+		if (nums[i] == 0 || i == len(nums)-1) && temp < 0 {
+			if endNegativeNum == 0 {
+				if i > 1 && temp/firstNegativeNum > maxNum {
+					maxNum = temp / firstNegativeNum
+				}
+			} else if firstNegativeNum > endNegativeNum {
+				maxNum = temp / firstNegativeNum
+			}
+			temp, firstNegativeNum, endNegativeNum = 1, 0, 0
 		}
 	}
 	return maxNum
--- leetcode/maxProduct_test.go
+++ leetcode/maxProduct_test.go
@@ -6,7 +6,9 @@ import (
 )
 
 func TestMaxProduct(t *testing.T) {
-	assert.Equal(t, 6, maxProduct([]int{2, -3, -2, 4}))
+	assert.Equal(t, 48, maxProduct([]int{2, -3, -2, 4}))
+
+	assert.Equal(t, 7000, maxProduct([]int{1, -3, 4, -5, 5, -7, 10}))
 
 	assert.Equal(t, 0, maxProduct([]int{-2, 0, -1}))
 }
